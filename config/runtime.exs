90b42ff00a8c2dbdbf34e4a83159211e455233b8
Leander <leanderfdo12@gmail.com>
2024-02-07 14:45:41 +0530

    Fix formatting

--- config/runtime.exs
+++ config/runtime.exs
@@ -49,7 +49,7 @@ if config_env() == :prod do
       """
 
   host =
-    System.get_env("PHX_HOST")||
+    System.get_env("PHX_HOST") ||
       raise "HOST_NAME not available"
 
   port = String.to_integer(System.get_env("PORT") || "4000")
@@ -107,14 +107,15 @@ if config_env() == :prod do
   # Also, you may need to configure the Swoosh API client of your choice if you
   # are not using SMTP. Here is an example of the configuration:
   #
-      config :events_url_shortener, EventsUrlShortener.Mailer,
-        adapter: Swoosh.Adapters.Sendgrid,
-        api_key: System.get_env("SENDGRID_API_KEY")
+  config :events_url_shortener, EventsUrlShortener.Mailer,
+    adapter: Swoosh.Adapters.Sendgrid,
+    api_key: System.get_env("SENDGRID_API_KEY")
+
   #
   # For this example you need include a HTTP client required by Swoosh API client.
   # Swoosh supports Hackney and Finch out of the box:
   #
-      config :swoosh, :api_client, Swoosh.ApiClient.Hackney
+  config :swoosh, :api_client, Swoosh.ApiClient.Hackney
   #
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 end
